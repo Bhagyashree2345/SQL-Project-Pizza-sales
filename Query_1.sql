48b5e27146e245aa6868e80c3de2a31a42401659
Bhagyashree2345 <111754818+Bhagyashree2345@users.noreply.github.com>
2024-06-14 14:43:02 -0500

    Add files via upload

---
+++ Query_1.sql
@@ -0,0 +1,7 @@
+-- Calculate the total revenue generated from pizza sales.
+
+select  
+sum(order_details.quantity *pizzas.price) AS total_sales
+
+from order_details join pizzas
+on pizzas.pizza_id = order_details.pizza_id
---
+++ SQL Query_2.sql
@@ -0,0 +1,5 @@
+-- Retrieve the total number of orders placed.
+
+SELECT *FROM orders;
+select count(order_id) from orders;
+
